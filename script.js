3cade25748d9152ff233d60fee463c9ea85ecb57
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2026-04-23 21:53:27 +0000

    Day 1

--- script.js
+++ script.js
@@ -12,20 +12,56 @@ console.log("Script started");
 */
 
 // TODO: Create an array of objects to store the questions, options, and answer
+let questions = [
+    {
+        question: "What is the capital of Arizona?",
+        option1: "Phoenix",
+        option2: "Tucson",
+        option3: "Denver",
+        option4: "Santa Fe",
+        answer: "Phoenix"
+    },
+    {
+        question: "What is 20 + 20 + 20 + 7?",
+        option1: "69",
+        option2: "420",
+        option3: "67",
+        option4: "47",
+        answer: "67"
+    }
+];
+
 
 // TODO: Declare an empty array that will hold the user's answers
 
 // TODO: Declare a variable to keep track of which question (index) we are on
+let qIndex = 0;
 
 function showNextQuestion() {
     // TODO: Get the current question
+    let question = questions[qIndex];
+    console.log(question);
     
     // TODO: Set the text of the question element
+    let questionElem = document.getElementById("question");
+    questionElem.innerText = question.question;
 
     // TODO: Set the text of each option element
+    let option1 = document.getElementById("option1text");
+    option1.innerText = question.option1;
+
+    let option2 = document.getElementById("option2text");
+    option2.innerText = question.option2;
+
+    let option3 = document.getElementById("option3text");
+    option3.innerText = question.option3;
+
+    let option4 = document.getElementById("option4text");
+    option4.innerText = question.option4;
 }
 
 // TODO: Call showNextQuestion to load the first question when the page loads
+showNextQuestion();
 
 function submitAnswer(event) {
     console.log("Submit clicked");
