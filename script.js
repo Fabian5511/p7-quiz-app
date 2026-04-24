0dda95016bcdeef6e098eff77937cf05cd55a35b
Fabian5511 <jfclujan5511@gmail.com>
2026-04-24 22:10:56 +0000

    progreaa

--- script.js
+++ script.js
@@ -33,11 +33,17 @@ let questions = [
 
 
 // TODO: Declare an empty array that will hold the user's answers
+let userAnswers = [];
 
 // TODO: Declare a variable to keep track of which question (index) we are on
 let qIndex = 0;
 
 function showNextQuestion() {
+    // Check answers
+    if (qIndex >= questions.length) {
+        checkAnswers();
+        return;
+    }
     // TODO: Get the current question
     let question = questions[qIndex];
     console.log(question);
@@ -66,6 +72,7 @@ showNextQuestion();
 function submitAnswer(event) {
     console.log("Submit clicked");
     // TODO: Prevent the form from reloading the page
+    event.preventDefault();
 
     // Get the radio button input elements
     let option1 = document.getElementById("option1");
@@ -74,18 +81,53 @@ function submitAnswer(event) {
     let option4 = document.getElementById("option4");
 
     // TODO: Get the current question
+    let question = questions[qIndex];
 
     // TODO: Use a conditional (if/else-if) to check which option was clicked and push the option text into the userAnswer array
+    if (option1.checked) {
+        userAnswers.push(question.option1);
+        option1.checked = false;
+    } else if (option2.checked) {
+        userAnswers.push(question.option2);
+        option2.checked = false;
+    } else if (option3.checked) {
+        userAnswers.push(question.option3);
+        option3.checked = false;
+    } else if (option4.checked) {
+        userAnswers.push(question.option4);
+        option4.checked = false;
+    }
+    else {
+        alert("Please select an option gng.");
+        return;
+    }
+   
 
     // TODO: Add one to the question index
+    qIndex++;
 
     // TODO: Load the next question
+    showNextQuestion();
 }
 
 function checkAnswers() {
     // TODO: Iterate through the user answers array and count how many are correct
+    let numCorrect = 0;
+    for (let i = 0; i < userAnswers; i++) {
+        let userAnswer = userAnswers[i];
+        let question = questions[i];
+        if (userAnswer == question.answer) {
+            numCorrect++;
+        }
+
+ }
+    }
 
     // TODO: Show an alert to the user with how many they got right out of the total
+    alert("You got " + numCorrect + " out of " + questions.length + " correct!");
 
     // TODO: Reset and start over
+     qIndex = 0;
+     userAnswers = [];
+     showNextQuestion();
 }
